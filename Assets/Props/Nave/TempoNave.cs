a4c0db9bfe606db84d141d52d55755f166e936a6
Ulisses Parola <ulissesmt@gmail.com>
2018-01-26 22:38:18 -0200

    tempo nave

---
+++ Assets/Props/Nave/TempoNave.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TempoNave : MonoBehaviour
+{
+    public float TempoDaFase;
+
+    private float _tempoFinal;
+    private Image _imagemNave;
+    private float _extensaoPosicaoNave;
+    private float _posicaoInicialNave;
+
+	void Start ()
+	{
+	    _extensaoPosicaoNave = 150;
+	    _posicaoInicialNave = transform.position.y;
+	    _tempoFinal = Time.time + TempoDaFase;
+	    _imagemNave = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Time.time <= _tempoFinal)
+	    {
+	        AtualizaPosicaoNave();
+	    }
+	}
+
+    private void AtualizaPosicaoNave()
+    {
+        float porcTempo = Time.time / TempoDaFase;
+        float variacaoPosicaoY = (_extensaoPosicaoNave * porcTempo);
+        Debug.Log(variacaoPosicaoY  + _posicaoInicialNave);
+        Vector2 novaPosicao = new Vector2(transform.position.x, _posicaoInicialNave + variacaoPosicaoY );
+        _imagemNave.transform.position = novaPosicao;
+    }
+}
